e868e3a76f209d27d3f078608e3826f4fc80c4b6
Vehikl Team <go@vehikl.com>
2022-02-08 17:05:36 -0500

    a new plugin is born, able to validate JSON structure

---
+++ src/main/java/J2AMenuAction.java
@@ -0,0 +1,93 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Menu action to replace a selection of characters with a fixed string.
+ *
+ * @see AnAction
+ */
+public class J2AMenuAction extends AnAction {
+
+    /**
+     * Replaces the run of text selected by the primary caret with a fixed string.
+     *
+     * @param e Event related to this action
+     */
+    @Override
+    public void actionPerformed(@NotNull final AnActionEvent e) {
+        // Get all the required data from data keys
+        // Editor and Project were verified in update(), so they are not null.
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+        final Document document = editor.getDocument();
+        // Work off of the primary caret to get the selection info
+        Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+
+        String selectedText = primaryCaret.getSelectedText();
+
+        int start = primaryCaret.getSelectionStart();
+        int end = primaryCaret.getSelectionEnd();
+
+        WriteCommandAction.runWriteCommandAction(project, () ->
+                document.replaceString(start, end, convertToArray(selectedText))
+        );
+        // De-select the text range that was just replaced
+        primaryCaret.removeSelection();
+    }
+
+    private String convertToArray(String text) {
+        return "";
+    }
+
+    /**
+     * Sets visibility and enables this action menu item if:
+     * <ul>
+     *   <li>a project is open</li>
+     *   <li>an editor is active</li>
+     *   <li>some characters are selected</li>
+     * </ul>
+     *
+     * @param e Event related to this action
+     */
+    @Override
+    public void update(@NotNull final AnActionEvent e) {
+        // Get required data keys
+        final Project project = e.getProject();
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        // Set visibility and enable only in case of existing project and editor and if a selection exists
+
+        if(project == null || editor == null) {
+            return;
+        }
+
+        Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+        String selectedText = primaryCaret.getSelectedText();
+
+        e.getPresentation().setEnabledAndVisible(
+                 editor.getSelectionModel().hasSelection() && isJSONValid(selectedText)
+        );
+    }
+
+    public static boolean isJSONValid(String test) {
+        try {
+            new JSONObject(test);
+        } catch (JSONException ex) {
+            try {
+                new JSONArray(test);
+            } catch (JSONException ex1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
